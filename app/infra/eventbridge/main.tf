f0303951d8d651a80df47f957818b865a4cd9f6c
Bianka Passos <biankatpas@gmail.com>
2025-02-24 11:29:02 -0300

    WIP: terraform

---
+++ app/infra/eventbridge/main.tf
@@ -0,0 +1,40 @@
+# Obtain the existing IAM role etl-role
+data "aws_iam_role" "etl_role" {
+  name = "LabRole"
+}
+
+# Obtain the existing state machine
+data "aws_sfn_state_machine" "bovespa_etl_state_machine" {
+  name = "bovespa_etl_state_machine"
+}
+
+# EventBridge rule to trigger when an object is created in your S3 bucket
+resource "aws_cloudwatch_event_rule" "s3_etl" {
+  name        = "etl-s3-rule"
+  description = "EventBridge rule to trigger the state machine when a file is uploaded to the bucket"
+  event_pattern = jsonencode({
+    "source": [
+      "aws.s3"
+    ],
+    "detail-type": [
+      "Object Created"
+    ],
+    "detail": {
+      "bucket": {
+        "name": [
+          "bovespa-etl-360494"
+        ]
+      }
+    }
+  })
+}
+
+# Permission for EventBridge to invoke the Step Functions state machine
+resource "aws_cloudwatch_event_target" "target_step_functions" {
+  rule     = aws_cloudwatch_event_rule.s3_etl.name
+  arn      = data.aws_sfn_state_machine.bovespa_etl_state_machine.arn
+  role_arn = data.aws_iam_role.etl_role.arn
+
+  input = jsonencode({})
+}
+
---
+++ app/infra/stepfunctions/main.tf
@@ -0,0 +1,58 @@
+# Obtain the existing IAM role etl-role
+data "aws_iam_role" "etl_role" {
+  name = "LabRole"
+}
+
+# Definition of the Step Functions state machine
+resource "aws_sfn_state_machine" "bovespa_etl_state_machine" {
+  name     = "bovespa-etl_state_machine"
+  role_arn = data.aws_iam_role.etl_role.arn
+
+  definition = jsonencode({
+    "Comment" : "Orchestration of ETL jobs using Step Functions",
+    "StartAt" : "ExtractJob",
+    "States" : {
+      "ExtractJob" : {
+        "Type" : "Task",
+        "Resource" : "arn:aws:states:::glue:startJobRun.sync",
+        "Parameters" : {
+          "JobName" : "extract"
+        },
+        "Catch" : [{
+          "ErrorEquals" : ["States.ALL"],
+          "Next" : "FailState"
+        }],
+        "Next" : "TransformJob"
+      },
+      "TransformJob" : {
+        "Type" : "Task",
+        "Resource" : "arn:aws:states:::glue:startJobRun.sync",
+        "Parameters" : {
+          "JobName" : "transform"
+        },
+        "Catch" : [{
+          "ErrorEquals" : ["States.ALL"],
+          "Next" : "FailState"
+        }],
+        "Next" : "LoadJob"
+      },
+      "LoadJob" : {
+        "Type" : "Task",
+        "Resource" : "arn:aws:states:::glue:startJobRun.sync",
+        "Parameters" : {
+          "JobName" : "load"
+        },
+        "Catch" : [{
+          "ErrorEquals" : ["States.ALL"],
+          "Next" : "FailState"
+        }],
+        "End" : true
+      },
+      "FailState" : {
+        "Type" : "Fail",
+        "Error" : "JobFailed",
+        "Cause" : "The job execution failed."
+      }
+    }
+  })
+}
